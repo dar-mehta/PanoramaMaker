475f9cfa76b4fe75c39c64664cb5884da77be7c5
Dar Mehta <dmehta97@gmail.com>
2016-01-01 17:51:41 -0500

    Added two image stitching
    Used opencv documentation and tutorials to add wrapPerspective function
    to stitch two images
    Cleaned up the code into appropriate functions

--- PanoramaMaker.cpp
+++ PanoramaMaker.cpp
@@ -17,40 +17,84 @@
 using namespace std;
 using namespace cv;
 
+void makePanorama(Mat, Mat);
+void findKeypointsAndDescriptors (Mat, Mat, vector<KeyPoint> &, Mat &, vector<KeyPoint> &, Mat &);
+void findMatchedPoints (vector<KeyPoint> &, Mat &, vector<KeyPoint> &, Mat &, vector<DMatch> &, vector<Point2f> &, vector<Point2f> &);
+void stitchTogether (Mat, Mat, vector<Point2f> &, vector<Point2f> &, Mat &);
+
 int main(int argc, const char * argv[]) {
     if (argc < 3){
         cout << "Invalid arguments. Input more images." << endl;
         return -1;
     }
-
-    vector<Mat> srcImg_Grayed, descriptors;
-    vector<vector<KeyPoint> > keypoints;
-    Mat drawnKeypoints;
-    vector<vector<DMatch> > matches;
-    namedWindow("Display Test");
     
-    for (int i = 1; i < argc; i++){
+    vector<Mat> srcImg_Grayed;
+    
+    for (int i = argc-1; i > 0; i--){
         Mat grayedImg;
-        cvtColor(imread(argv[i]), grayedImg, CV_RGB2GRAY);
-        srcImg_Grayed.push_back(grayedImg);
+        //cvtColor(imread(argv[i]), grayedImg, CV_RGB2GRAY);
+        srcImg_Grayed.push_back(imread(argv[i]));
     }
     
-    int minHessian = 1000;
-    Ptr<xfeatures2d::SURF> surfDetector = xfeatures2d::SURF::create(minHessian);
-    surfDetector->detect(srcImg_Grayed, keypoints);
+    makePanorama(srcImg_Grayed[0], srcImg_Grayed[1]);
     
-    Ptr<xfeatures2d::SurfDescriptorExtractor> surfExtractor = xfeatures2d::SurfDescriptorExtractor::create();
-    surfExtractor->compute(srcImg_Grayed, keypoints, descriptors);
-    
-    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("FlannBased");
-    for (int i = 0; i < argc - 2; i ++){
-        cout << "Hello " << i << endl;
-        matcher->knnMatch(descriptors[i], descriptors[i+1], matches, 1);
-        cout << "End " << i << endl;
-    }
+}
 
-    drawMatches(srcImg_Grayed[0], keypoints[0], srcImg_Grayed[1], keypoints[1], matches, drawnKeypoints, Scalar(0, 0, 255), Scalar(255, 0, 0));
+void makePanorama(Mat imgObj, Mat imgScene){
     
-    imshow("Display Test", drawnKeypoints);
+    vector<KeyPoint> objKeypoints, sceneKeypoints;
+    Mat drawnKeypoints, objDescriptors, sceneDescriptors, result;
+    vector<DMatch> matches;
+    vector<Point2f> objMatchedPoints, sceneMatchedPoints;
+    
+    findKeypointsAndDescriptors(imgObj, imgScene, objKeypoints, objDescriptors, sceneKeypoints, sceneDescriptors);
+    
+    findMatchedPoints(objKeypoints, objDescriptors, sceneKeypoints, sceneDescriptors, matches, objMatchedPoints, sceneMatchedPoints);
+    
+    stitchTogether(imgObj, imgScene, objMatchedPoints, sceneMatchedPoints, result);
+    
+    imshow("Display Test", result);
     waitKey(0);
 }
+
+//Finding keypoints & descriptors using SURF
+void findKeypointsAndDescriptors (Mat imgO, Mat imgS, vector<KeyPoint> &oK, Mat &oD, vector<KeyPoint> &sK, Mat &sD){
+    
+    const int minHessian = 2500;
+    Ptr<xfeatures2d::SURF> detectorAndExtractor = xfeatures2d::SURF::create(minHessian);
+    
+    detectorAndExtractor->detectAndCompute(imgO, noArray(), oK, oD);
+    detectorAndExtractor->detectAndCompute(imgS, noArray(), sK, sD);
+}
+
+//Finding matches using FLANN
+//Filtering matches using David Lowe's Algorithm from his paper "Distinctive Image Features
+//from Scale-Invariant Keypoints"
+void findMatchedPoints (vector<KeyPoint> &oK, Mat &oD, vector<KeyPoint> &sK, Mat &sD, vector<DMatch> &m, vector<Point2f> &oP, vector<Point2f> &sP){
+    
+    const float lowe_ratio = 0.45;
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("FlannBased");
+    vector<DMatch> goodMatches;
+    goodMatches.reserve(m.size());
+    
+    matcher->match(oD, sD, m);
+    for (int i = 0; i < m.size(); i++){
+        if (m[i].distance < lowe_ratio * m[i+1].distance){
+            goodMatches.push_back(m[i]);
+        }
+    }
+    
+    for (int i = 0; i < goodMatches.size(); i++){
+        oP.push_back(oK[goodMatches[i].queryIdx].pt);
+        sP.push_back(sK[goodMatches[i].trainIdx].pt);
+    }
+}
+
+void stitchTogether (Mat imgO, Mat imgS, vector<Point2f> &oP, vector<Point2f> &sP, Mat & result){
+    
+    Mat homographyMat = findHomography(oP, sP, CV_RANSAC);
+    warpPerspective(imgO,result,homographyMat,cv::Size(imgO.cols+imgS.cols,imgO.rows));
+    cv::Mat half(result,cv::Rect(0,0,imgS.cols,imgS.rows));
+    imgS.copyTo(half);
+}
+
