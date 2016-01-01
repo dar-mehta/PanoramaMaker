db9c898ce9d853b81c9f2a5048952d14079dac60
Dar Mehta <dmehta97@gmail.com>
2016-01-01 14:24:21 -0500

    Keypoints, descriptors and matching
    Find and store keypoints and descriptors of each image into vector
    objects. Segmentation faulting at line 48, found using flags

--- PanoramaMaker.cpp
+++ PanoramaMaker.cpp
@@ -23,7 +23,10 @@ int main(int argc, const char * argv[]) {
         return -1;
     }
 
-    vector<Mat> srcImg_Grayed;
+    vector<Mat> srcImg_Grayed, descriptors;
+    vector<vector<KeyPoint> > keypoints;
+    Mat drawnKeypoints;
+    vector<vector<DMatch> > matches;
     namedWindow("Display Test");
     
     for (int i = 1; i < argc; i++){
@@ -31,4 +34,23 @@ int main(int argc, const char * argv[]) {
         cvtColor(imread(argv[i]), grayedImg, CV_RGB2GRAY);
         srcImg_Grayed.push_back(grayedImg);
     }
+    
+    int minHessian = 1000;
+    Ptr<xfeatures2d::SURF> surfDetector = xfeatures2d::SURF::create(minHessian);
+    surfDetector->detect(srcImg_Grayed, keypoints);
+    
+    Ptr<xfeatures2d::SurfDescriptorExtractor> surfExtractor = xfeatures2d::SurfDescriptorExtractor::create();
+    surfExtractor->compute(srcImg_Grayed, keypoints, descriptors);
+    
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("FlannBased");
+    for (int i = 0; i < argc - 2; i ++){
+        cout << "Hello " << i << endl;
+        matcher->match(descriptors[i], descriptors[i+1], matches[i]);
+        cout << "End " << i << endl;
+    }
+
+    drawMatches(srcImg_Grayed[0], keypoints[0], srcImg_Grayed[1], keypoints[1], matches, drawnKeypoints, Scalar(0, 0, 255), Scalar(255, 0, 0));
+    
+    imshow("Display Test", drawnKeypoints);
+    waitKey(0);
 }
